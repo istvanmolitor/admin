c32aceb00cfdfbfec4e839e8b935a3bdbea89a1a
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-01-22 22:08:43 +0100

    refactor

--- resources/js/routes.ts
+++
@@ -1,6 +0,0 @@
-import { index as dashboardIndex } from '../../../../resources/js/actions/Molitor/Admin/Controllers/DashboardController';
-
-/**
- * Dashboard route
- */
-export const dashboard = dashboardIndex;
